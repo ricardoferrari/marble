dfe32c16065ad7670152e5945b8778bdb0e1508b
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2024-04-06 22:15:34 -0300

    test(jest): initial setup

---
+++ setup-jest.ts
@@ -0,0 +1,7 @@
+import "jest-preset-angular/jest-preset.js";
+import 'zone.js';
+
+import { TestBed } from "@angular/core/testing";
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
