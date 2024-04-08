50005e4fedb76dcac7ee5c8f3b6f36a5ad532006
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2024-04-07 23:03:51 -0300

    test(marble): added marble testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,97 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, flush } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TestScheduler } from 'rxjs/testing';
+import { throttleTime } from 'rxjs';
+
+const testScheduler = new TestScheduler((actual, expected) => {
+  // asserting the two objects are equal - required
+  // for TestScheduler assertions to work via your test framework
+  // e.g. using chai.
+  expect(actual).toEqual(expected);
+});
 
 describe('AppComponent', () => {
+
+  let fixture: any;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have the 'marble' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('marble');
+    expect(component.title).toEqual('marble');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, marble');
   });
+
+  it('generates the stream correctly', () => {
+    testScheduler.run((helpers) => {
+      const { cold, time, expectObservable, expectSubscriptions } = helpers;
+      const e1 = cold(' -a--b--c---|');
+      const e1subs = '  ^----------!';
+      const t = time('   ---|       '); // t = 3
+      const expected = '-a-----c---|';
+
+      expectObservable(e1.pipe(throttleTime(t))).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
+  });
+
+  it('when setFlag was called, should load data and set flag', () => {
+    testScheduler.run((helpers) => {
+      const { cold, flush } = helpers;
+      const flags = cold('a--b--a', { a: 'fake', b: 'notfake' });
+
+      (component as any).setFlag(flags);
+
+      flush();
+      expect(component.fakeFlag).toBe(true);
+    });
+  });
+
+  it('when setFlag was called with error, should not set flag', () => {
+    testScheduler.run((helpers) => {
+      const { cold, flush } = helpers;
+      const flags = cold('#');
+
+      (component as any).setFlag(flags);
+      flush();
+
+      expect(component.fakeFlag).toBe(false);
+    });
+  });
+
+
+  it('when ngOnDestroy was called, should unsubscribe', () => {
+    testScheduler.run((helpers) => {
+      const { hot, expectSubscriptions, flush } = helpers;
+      const flags = hot('                  a--b--a');
+      const destroys = hot('               ---a|');
+      const expectSubs = '-----------------^--! ';
+
+
+      (component as any).service$ = flags;
+      (component as any).destroy$ = destroys;
+      (component as any).ngOnInit();
+      flush();
+      (component as any).ngOnDestroy();
+
+      expectSubscriptions(flags.subscriptions).toBe(expectSubs);
+    });
+  });
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable, Subject, Subscription, take, takeUntil, tap } from 'rxjs';
 
+class FakeService {
+  private value: Subject<string> = new Subject<string>();
+
+  get(): Observable<string> {
+    return this.value.asObservable();
+  }
+
+  loadData() {
+    this.value.next('fake');
+  }
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -8,6 +20,44 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'marble';
+export class AppComponent implements OnInit, OnDestroy{
+  public title = 'marble';
+  private service: FakeService;
+  private destroy$: Subject<void> = new Subject<void>();
+  private service$: any;
+  public fakeFlag: boolean = false;
+
+
+  public result: string = '';
+
+  constructor() {
+    this.service = new FakeService();
+    this.service$ = this.service.get();
+  }
+
+  public ngOnInit(): void {
+    this.setFlag(this.service$);
+    this.service.loadData();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private setFlag(obs: Observable<string>) {
+    obs.pipe(
+      takeUntil(this.destroy$),
+      tap((result) => (this.result = result)),
+      tap(console.log)
+    ).subscribe({
+      next: (result) => {
+        this.fakeFlag = (result === 'fake');
+        console.log('Resultado Flag: ', this.fakeFlag, result);
+      },
+      error: (error) => console.log('Erro:', error),
+      complete: () => console.log('complete')
+    });
+  }
+
 }
